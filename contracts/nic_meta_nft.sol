8598d4611bb2060422b928513356d568fcbbe2b7
sunlong <silence.sun@paytm.com>
2022-05-07 17:47:41 +0800

    update max balance

--- contracts/nic_meta_nft.sol
+++ contracts/nic_meta_nft.sol
@@ -15,7 +15,7 @@ contract NicMeta is ERC721Enumerable, Ownable {
     // Constants
     uint256 public constant MAX_SUPPLY = 100;
     uint256 public mintPrice = 0.00003 ether;
-    uint256 public maxBalance = 1;
+    uint256 public maxBalance = 100;
     uint256 public maxMint = 100;
 
     string baseURI;
